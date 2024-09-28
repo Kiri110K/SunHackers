3566470174cd25ce88b7e632a4575216dd9b1a15
Kiri110K <kirill.a.chernov@gmail.com>
2024-09-28 15:15:51 -0700

    Populated some files

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Termina : MonoBehaviour
+public class Terminal : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,3 +16,6 @@ public class Termina : MonoBehaviour
         
     }
 }
+
+
+
