ae46b0b2859c3937f3604cdb402d5da4f22d4dcc
Kiri110K <kirill.a.chernov@gmail.com>
2024-09-28 22:20:06 -0700

    Monitors!!!!

--- Assets/Scripts/MainMonitor.cs
+++ Assets/Scripts/MainMonitor.cs
@@ -6,24 +6,34 @@ using TMPro;
 public class MainMonitor : MonoBehaviour
 {
     public TextMeshPro monitor;
+    public TMP_InputField userInputField; // The InputField for user input
+
     // Start is called before the first frame update
     void Start()
     {
         // Initialize the text displayed on the monitor
         monitor.text = "Terminal is now running...";
+        
+        // Activate input field when the game starts
+        userInputField.ActivateInputField();
     }
 
     // Update is called once per frame
-    int counter = 0;
+    
     int textCounter = 1;
     void Update()
+    {        
+        // Keep the Input Field active (Optional)
+        if (!userInputField.isFocused)
+        {
+            userInputField.ActivateInputField();
+        }
+    }
+
+    void FixedUpdate() 
     {
-        counter += 1;
-        counter %= 30;
-        if (counter == 29) {
             UpdateMonitor(textCounter.ToString());
             textCounter += 1;
-        }
     }
 
         // Function to update the monitor text
